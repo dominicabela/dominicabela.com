2701c18ee4bfd0b122d5720d59341448462c2a71
Dominic Abela <d.j.m.abela@gmail.com>
2019-08-29 01:49:05 -0400

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,7 @@ import React from 'react'
 
 class Footer extends React.Component {
     render() {
-        return (
-            <div></div>
-        )
+        return (<div></div>)
     }
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -72,23 +72,7 @@ class HomeIndex extends React.Component {
                     </section>
 
                     <section id="four">
-                        <header className="major">
-                            <h2>Contact</h2>
-                        </header>
-                        <div className="row">
-                            <div className="12u 12u$(small)">
-                                <form method="post" action="#" netlify>
-                                    <div className="row uniform 50%">
-                                        <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
-                                        <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
-                                        <div className="12u"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
-                                    </div>
-                                </form>
-                                <ul className="actions">
-                                    <li><input type="submit" value="Send Message" /></li>
-                                </ul>
-                            </div>
-                        </div>
+                        <h5>© {new Date().getFullYear()} Dominic Abela</h5>
                     </section>
 
                 </div>
