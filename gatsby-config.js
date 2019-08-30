ea64fe81be2efa3115cb21f557e99e8e23904305
Dominic Abela <d.j.m.abela@gmail.com>
2019-08-30 01:44:59 -0400

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,26 @@ module.exports = {
         icon: 'src/assets/images/site-icon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-146790666-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: false,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "example.com",
+      },
+    },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
   ],
