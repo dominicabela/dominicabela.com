b83414737cb1d777d05e88cfd24e3d9493ebacc3
Dominic Abela <d.j.m.abela@gmail.com>
2019-09-09 16:24:02 -0400

    update cookieDomain

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
         // Any additional optional fields
         sampleRate: 5,
         siteSpeedSampleRate: 10,
-        cookieDomain: "example.com",
+        cookieDomain: "www.dominicabela.com",
       },
     },
     'gatsby-plugin-sass',
