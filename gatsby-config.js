7bd08058bd1bfd9373eb425a82f9fc02c381cf0e
Dominic Abela <d.j.m.abela@gmail.com>
2019-09-11 00:47:16 -0400

    update google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,12 @@ module.exports = {
     description: 'Developer Portfolio for Dominic Abela',
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-146790666-1",
+      },
+    },
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-manifest`,
@@ -16,26 +22,6 @@ module.exports = {
         theme_color: '#b2b2b2',
         display: 'minimal-ui',
         icon: 'src/assets/images/site-icon.png', // This path is relative to the root of the site.
-      },
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-146790666-1",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: false,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Any additional optional fields
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
-        cookieDomain: "www.dominicabela.com",
       },
     },
     'gatsby-plugin-sass',
